0f94d85d4f340a60f89e7bb629c6c58e98fb726b
Dima <livitorskipunovich@gmail.com>
2022-10-08 12:48:29 +0300

    Update: Add "Завдання 4"

---
+++ hw.py
@@ -0,0 +1,17 @@
+try:
+    sum = 0
+    while True:
+        num = int(input('input->'))
+        if num != 7:
+            if num > 0:
+                print("Number is positive")
+            if num < 0:
+                print("Number is negative")
+            if num == 0:
+                print("Number is equal to zero")
+            sum+=num
+        else:
+            print('sum = ', sum)
+            break
+except Exception as ex :
+    print(f'Erorr information: {ex}')
